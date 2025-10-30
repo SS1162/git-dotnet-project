3e1f8d9ed35134b272c2aeae7c8001b2f0d711ac
SS1162 <s058329162@gmail.com>
2025-10-30 18:02:16 +0200

    add antribute

--- WebApiShope/WebApiShope/UserEnter.cs
+++ WebApiShope/WebApiShope/UserEnter.cs
@@ -1,9 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace WebApiShope
 {
     public class LoginUser
     {
 
         public int UserID { get; set; }
+        [EmailAddress]
         public string UserName { get; set; }
         public string UserPassward { get; set; }
 
--- WebApiShope/WebApiShope/users.cs
+++ WebApiShope/WebApiShope/users.cs
@@ -1,11 +1,15 @@
-﻿namespace WebApiShope
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiShope
 {
     public class Users
     {
+        //[Required]
         public int UserID { get; set; }
+        
         public string UserName { get; set; }
         public string UserPassward { get; set; }
-
+         [EmailAddress]
         public string UserFirstName { get; set; }
 
         public string UserLastName { get; set; }
